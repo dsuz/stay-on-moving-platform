7a0f9aad63d5d7c4bb0d453a198ad4fb60458f91
Daisuke Suzuki <bboydaisuke@gmail.com>
2017-10-12 10:36:33 +0900

    initial commit

---
+++ Assets/MovingPlatformController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 左右に動き、上に乗れるオブジェクトに追加する
+/// </summary>
+[RequireComponent(typeof(Collider2D))]
+public class MovingPlatformController : MonoBehaviour
+{
+    /// <summary>振り幅</summary>
+    public float m_amplitude = 1f;
+
+    public float m_speed = 2.0f;
+    private float m_timer;
+    private Vector3 m_initialPosition;
+
+    private void Start()
+    {
+        m_initialPosition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        // オブジェクトを左右に揺らす
+        m_timer += Time.deltaTime * m_speed;
+        float posXSin = Mathf.Sin(m_timer) * m_amplitude;
+        Vector3 pos = m_initialPosition + new Vector3(posXSin, 0, 0);
+        transform.position = pos;
+    }
+
+    // プレイヤーが上に乗った時、それを子オブジェクトとすることによりプレイヤーはオブジェクトと一緒に動く
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            collision.collider.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    // プレイヤーがオブジェクトから離れた時は、親子関係を解除する
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            collision.collider.gameObject.transform.SetParent(null);
+        }
+
+    }
+}
