565cb7a2dc6d425fc12ef094f99ed38739921066
bboydaisuke <bboydaisuke@gmail.com>
2018-04-29 14:41:13 +0900

    床の動きを横のみから縦にも対応した

--- Assets/MovingPlatformController.cs
+++ Assets/MovingPlatformController.cs
@@ -9,11 +9,12 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class MovingPlatformController : MonoBehaviour
 {
-    /// <summary>振り幅</summary>
-    public float m_amplitude = 1f;
-
+    /// <summary>振り幅（横）</summary>
+    public float m_amplitude_x = 1f;
+    /// <summary>振り幅（縦）</summary>
+    public float m_amplitude_y = 0f;
+    /// <summary>動く速さ</summary>
     public float m_speed = 2.0f;
-    public bool m_moveLeftFirst = false;
     private float m_timer;
     private Vector3 m_initialPosition;
 
@@ -22,16 +23,15 @@ public class MovingPlatformController : MonoBehaviour
         m_initialPosition = transform.position;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        // オブジェクトを左右に揺らす
+        // オブジェクトを回す
         m_timer += Time.deltaTime * m_speed;
-        float posXSin = Mathf.Sin(m_timer) * m_amplitude;
+        float posX = Mathf.Sin(m_timer) * m_amplitude_x;
+        float posY = Mathf.Cos(m_timer) * m_amplitude_y;
+
         Vector3 pos = m_initialPosition;
-        if (m_moveLeftFirst)
-            pos -= new Vector3(posXSin, 0, 0);
-        else
-            pos += new Vector3(posXSin, 0, 0);
+        pos = pos + new Vector3(posX, posY, 0);
         transform.position = pos;
     }
 
